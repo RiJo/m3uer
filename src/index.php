c66cb45435584c852f1f18a1fa5d821b07ef168a
RiJo <rajoo@mail.com>
2011-05-12 09:59:43 +0200

    initial fullscreen data loader message

--- src/index.php
+++ src/index.php
@@ -32,14 +32,51 @@ echo "\n        <script type=\"text/javascript\" src=\"".EXTJS_PATH."/ext-all-de
 echo "\n        <script type=\"text/javascript\" src=\"ext.js\"></script>";
 echo "\n    </head>";
 echo "\n    <body>";
+echo "\n        <div id='loading-mask'></div>";
+echo "\n        <div id='loading'>";
+echo "\n            <span id='loading-message'>Loading. Please wait...</span>";
+echo "\n        </div>";
 echo "\n        <div id='container'>";
 echo "\n            <div class='content' id='header'>".APPLICATION_NAME." v.".APPLICATION_VERSION."</div>";
 echo "\n            <div class='content' id='messages'></div>";
 echo "\n            <div class='content' id='tree'></div>";
 //echo "\n            <div class='content' id='footer'><a href=\"unit_tests.php\">Unit tests</a></div>";
+
+// Load filesystem
+echo "\n            <script type='text/javascript'>";
+echo "\n                 document.getElementById('loading-message').innerHTML = 'Loading filesystem...';";
+echo "\n            </script>";
+
+
+// Load extjs components
+echo "\n            <script type='text/javascript'>";
+echo "\n                 document.getElementById('loading-message').innerHTML = 'Loading graphics...';";
+echo "\n            </script>";
 echo "\n            <script type='text/javascript'>";
 echo "\n                javascript:render('".ROOT_DIRECTORY."', '".((empty($_GET['playlist'])) ? '' : $_GET['playlist'])."');";
 echo "\n            </script>";
+
+// Fade out loader
+echo "\n            <script type='text/javascript'>";
+echo "\n            Ext.onReady(function(){";
+echo "\n                var loadingMask = Ext.get('loading-mask');";
+echo "\n                var loading = Ext.get('loading');";
+echo "\n                //  Hide loading message";
+echo "\n                loading.fadeOut({ duration: 0.2, remove: true });";
+echo "\n                //  Hide loading mask";
+echo "\n                loadingMask.setOpacity(0.9);";
+echo "\n                loadingMask.shift({";
+echo "\n                    xy: loading.getXY(),";
+echo "\n                    width: loading.getWidth(),";
+echo "\n                    height: loading.getHeight(),";
+echo "\n                    remove: true,";
+echo "\n                    duration: 1,";
+echo "\n                    opacity: 0.1,";
+echo "\n                    easing: 'bounceOut'";
+echo "\n                });";
+echo "\n            });";
+echo "\n            </script>";
+
 echo "\n        </div>";
 echo "\n    </body>";
 echo "\n</html>";
